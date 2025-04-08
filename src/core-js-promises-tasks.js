28783f0ae852eef27a00e6ee73b7f3deb8695eb7
Artem_PC <taftoos2@gmail.com>
2025-04-08 17:02:14 +0300

    feat: solve queuePromises test

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -137,8 +137,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((chain, promise) => {
+    return chain.then((string) => {
+      return promise.then((number) => `${string}${number}`);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
